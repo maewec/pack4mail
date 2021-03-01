87ddc8161cc5ce90ecc53d5504768ed49b7e7c1a
maewec <dmitrij_n@inbox.ru>
2021-03-01 18:57:42 +0300

    Выполнена функция рекурсивно выводящая список файлов

---
+++ pack4mail.py
@@ -0,0 +1,27 @@
+import zipfile, os, glob
+
+path = r'/home/dima/working/python/games/snake/'
+
+def tree(path):
+    p = []
+    for i in os.listdir(path):
+        new_path = os.path.join(path, i)
+        if os.path.isdir(new_path):
+            p.extend(tree(new_path))
+        else:
+            p.append(new_path)
+    return p
+
+def main():
+    os.chdir(path)
+    files = tree('.')
+    print('List of files:')
+    for i in files:
+        print(i)
+    with zipfile.ZipFile('archive.zip', mode='w',
+                         compression=zipfile.ZIP_DEFLATED) as zf:
+        pass
+
+if __name__ == '__main__':
+    main()
+
