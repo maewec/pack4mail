8c87353c94ce051457f06a8d0ca7618b8e682e8a
maewec <dmitrij_n@inbox.ru>
2021-03-02 07:34:32 +0300

    Написал функцию запаковки, вынес изменение файла в отдельную функцию

--- pack4mail.py
+++ pack4mail.py
@@ -1,6 +1,5 @@
 import zipfile, os, glob
 
-path = r'/home/dima/working/python/games/snake/'
 
 def tree(path):
     p = []
@@ -12,21 +11,33 @@ def tree(path):
             p.append(new_path)
     return p
 
-def pack(list_files):
-    with zipfile.ZipFile('archive.zip', mode='w',
+def pack(list_files, name='archive'):
+    with zipfile.ZipFile(name+'.zip', mode='w',
                          compression=zipfile.ZIP_DEFLATED) as zf:
         for fil in list_files:
-            zf.write(fil, arcname=fil+'_')
+            zf.write(fil, arcname=new_name(fil))
     return 0
+
+def new_name(name):
+    return name + '_'
+
+def old_name(name):
+    return name[0:-1]
 
 
 def main():
+    path = r'/home/dima/working/python/games/snake/'
+    path = os.path.abspath(path)
+    name_archive = os.path.split(path)[-1]
     os.chdir(path)
     files = tree('.')
     print('List of files:')
     for i in files:
         print(i)
-    pack(list_files)
+    print('Имя архива', name_archive)
+
+    pack(files, name=name_archive)
+
 
 
 
